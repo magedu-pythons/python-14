6f4f4f56e5e29a3cef6be587d7b962b2106ec83b
houdinis <houdinis@163.com>
2018-07-14 21:52:53 +0800

    acitve_code

--- chengdexing/week2/active_code.py
+++ chengdexing/week2/active_code.py
@@ -4,14 +4,13 @@ import random
 lst_code = []
 active_code = ''
 value = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-num = 0
 for n in range(50):
+    num = 0
     for i in range(25):
         num += 1
         active_code += random.choice(value)
         if num % 5 == 0 and num != 25:
             active_code += '-'
-    num = 0
     lst_code.append(active_code)
     active_code = ''
 
