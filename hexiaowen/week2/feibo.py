bc69882c1190b2fc5bc32fd7d1d6bd30146b6661
XiaowenHEinchina <786551691@qq.com>
2018-07-14 09:59:08 +0800

    test

--- hexiaowen/week2/feibo.py
+++ hexiaowen/week2/feibo.py
@@ -1,9 +1,7 @@
-count=0
-feibo_list=[]
-i=1
-for i in range(1,50):
-    count+=i
-    if count>100:
+feibo_list=[1,1]
+for i in range(50):
+    new=feibo_list[i]+feibo_list[i+1]
+    if new>100:
         break
-    feibo_list.append(count)
+    feibo_list.append(new)
 print(feibo_list)
