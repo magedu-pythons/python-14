d19dd455123ccff792b7cc9a98d213fe594439d3
dywwen <359989190@qq.com>
2018-08-03 17:17:36 +0800

    week5 homework

---
+++ dongyanwen/week5/01-alist.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#.Author:Dyw
+import random
+alist=[1,2,3,4,5]
+random.shuffle(alist)
+print(alist)
---
+++ dongyanwen/week5/02-commodity.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+#.Author:Dyw
+import random
+print(random.choices(['socks','shoes','slipper','necklace'],[10,20,30,40],k=1))
