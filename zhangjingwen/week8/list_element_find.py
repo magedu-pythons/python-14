61edd4e5fec41f8dea6f59cc714e7ed9477b497e
ZhangJingWenF <xdzhangjingwen@163.com>
2018-08-26 20:50:17 +0800

    update the work

--- zhangjingwen/week8/list_element_find.py
+++ zhangjingwen/week8/list_element_find.py
@@ -8,69 +8,35 @@ def element_find(src: list, num: int):
     new.append(num/2)
     new.sort()
     index = new.index(num/2)
+    same = [x for x in new[index+1:] if x == num/2]
+    larger = [x for x in new[index+1:] if x > num/2]
 
     while len(src) >= 2:
-        result = []
-        count = 0       # 迭代次数统计
+        count1 = 0
+        lst = []
         for i in range(index):
-            for j in range(index+1, len(new)):
-                count += 1
-                if new[i]+new[j] == num:
-                    result.append((new[i], new[j]))
-        return (result, count) if result else None
+            for j in larger:
+                count1 += 1
+                if j > num-new[i]:
+                    break                                         # 提前退出迭代
+                if new[i] + j == num:
+                    lst.append((new[i], j))
+        # lst = [(new[i], j) for i in range(index) for j in larger if new[i] + j == num]
+
+        if len(same) >= 2:                                        # 等于中值情况的处理
+            for _ in range(len(same)-1):
+                lst.append((same[0], same[0]))
+        count = count1 + (len(same)-1 if len(same) >= 2 else 0)   # 迭代次数统计
+        return (lst, count) if lst else None
     else:
         return
 
 
 '''test'''
-print(element_find([-1, -2, 0, 8, 5, 7, 9, 2, 5, 25, 14, 3], 8))    # ([(-1, 9), (0, 8), (3, 5), (3, 5)], 35)
+print(element_find([-1, -2, 0, 8, 5, 7, 9, 2, 5, 25, 14, 3], 8))    # ([(-1, 9), (0, 8), (3, 5), (3, 5)], 23)
 print(element_find([-1, -2, 0, 8, 5, 7, 9], 8))                     # ([(-1, 9), (0, 8)], 12)
 print(element_find([-1, -2, 0, 8, 5, 7, 9], 10))                    # None
 print(element_find([2], 10))                                        # None
-print(element_find([1, 5, 2, 7, 4, 9], 11))                         # ([(2, 9), (4, 7)], 8)
+print(element_find([1, 5, 2, 7, 4, 9], 11))                         # ([(2, 9), (4, 7)], 7)
+print(element_find([2, 2, 2, 2], 4))                                # ([(2, 2), (2, 2), (2, 2)], 3)
 
-# -----------------------------------------分割线------------------------------------------------------
-# 方法二
-'''思路：利用0、num/2、num三个数将原列表切分成四段，0之前的与num之后的组合迭代；0~num/2的与num/2~num组合迭代'''
-
-
-def element_find(src: list, num: int):
-    while len(src) >= 2:
-        new = src.copy()
-        new.extend([0, num / 2, num])
-        new.sort()
-        for x, y in enumerate(new):
-            if y > 0:
-                index1 = x-1
-                break
-        index2 = new.index(num / 2)
-        index3 = new.index(num)
-
-        count1 = 0
-        count2 = 0          # 迭代次数统计
-        result = []
-        if new[:index1] and new[index3+1:]:
-            for i in range(index1):
-                for j in range(index3+1, len(new)):
-                    count1 += 1
-                    if new[i]+new[j] == num:
-                        result.append((new[i], new[j]))
-
-        if new[index1+1:index2] and new[index2+1:index3]:
-            for m in range(index1+1, index2):
-                for n in range(index2+1, index3):
-                    count2 += 1
-                    if new[m]+new[n] == num:
-                        result.append((new[m], new[n]))
-
-        return (result, count1+count2) if result else None
-    else:
-        return
-
-
-'''test  总结：在列表规模较大、正负数均存在时，方法二能够减少更多的迭代次数，效率更高'''
-print(element_find([-1, -2, 0, 8, 5, 7, 9, 2, 5, 25, 14, 3], 8))   # ([(-1, 9), (0, 8), (3, 5), (3, 5)], 18)
-print(element_find([-1, -2, 0, 8, 5, 7, 9], 8))                    # ([(-1, 9), (0, 8)], 6)
-print(element_find([-1, -2, 0, 8, 5, 7, 9], 10))                   # None
-print(element_find([2], 10))                                       # None
-print(element_find([1, 5, 2, 7, 4, 9], 11))                        # ([(2, 9), (4, 7)], 8)
